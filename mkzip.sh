fd60c076b42c6a54576b9eda9490fc870fca14a1
Chris Smith <chris@zxdesign.info>
2022-04-06 17:56:17 +0100

    Adds v1.0.1 of files (XML is from upstream Amiberry commit 727ef6d

---
+++ mkzip.sh
@@ -0,0 +1,12 @@
+if [ $# -eq 0 ]
+then
+    echo "Usage: $(basename $0) <version>"
+    echo
+    echo "Example: mkzip.sh 1.3.2"
+    exit 1
+fi
+
+V=$1
+F=THEA500-WHDLoad-package-${V}.zip
+zip -r ${F} THEA500
+sha256sum ${F}
